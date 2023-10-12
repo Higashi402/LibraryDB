3d3b4914497e774d98fe1fec3741aebb43108691
LizzardKing1967 <127925968+LizzardKing1967@users.noreply.github.com>
2023-10-12 17:52:04 +0300

    g

--- src/main/java/ru/rsreu/library/commands/FrontController.java
+++ src/main/java/ru/rsreu/library/commands/FrontController.java
@@ -9,9 +9,18 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+/**
+ *
+ */
 @WebServlet(name = "FrontControllerServlet")
 public class FrontController extends HttpServlet {
 
+    /**
+     * @param req
+     * @param resp
+     * @throws ServletException
+     * @throws IOException
+     */
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         FrontCommand command = getCommand(req);
