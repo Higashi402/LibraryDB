09fe8a2edad381b69205ad8c648449510755d188
Higashi402 <144343944+Higashi402@users.noreply.github.com>
2023-10-12 17:44:35 +0300

    3

--- src/Factory/DAOAbstractFactory.java
+++
@@ -1,9 +0,0 @@
-package Factory;
-
-public abstract class DAOAbstractFactory {
-	public static DAOAbstractFactory getInstance(DBType dbType) {
-		DAOAbstractFactory result = dbType.getDAOAbstractFactory();
-		return result;
-	}
-
-}
